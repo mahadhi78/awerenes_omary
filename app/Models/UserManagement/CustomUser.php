6cd02859043430b5191d45959fb614f4e9b2a1d4
mahadhi78 <mahadhiramadhan@gmail.com>
2024-06-01 19:23:40 +0300

    update username

--- app/Models/UserManagement/CustomUser.php
+++ app/Models/UserManagement/CustomUser.php
@@ -34,7 +34,7 @@ class CustomUser extends Model
 		'middlename' => 'required',
 		'lastname' => 'required',
 		'phone_number' => 'required',
-		'username' => 'required|unique:users|min:8|max:50',
+		'username' => 'required|unique:users|min:5|max:50',
 		'email' => ['required', 'string', 'email', 'unique:users', 'regex:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/'],
 	];
 
